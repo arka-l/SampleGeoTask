6c695be203e6ad12237cbf04cc22b17a79a55258
arka-l <soydemoscu@gmail.com>
2014-10-06 15:43:28 +0400

    Some minor changes

--- app/src/main/java/com/example/vall/geotask/MyActivity.java
+++ app/src/main/java/com/example/vall/geotask/MyActivity.java
@@ -1,36 +1,97 @@
 package com.example.vall.geotask;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 
 public class MyActivity extends ActionBarActivity {
+
+    private static final int REQUEST_CODE = 1;
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
+        mButton = (Button) findViewById(R.id.main_activity_button);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onStart() {
+        super.onStart();
+        if (isGoogleServicesOk()) {
+            mButton.setEnabled(true);
+        } else {
+            mButton.setEnabled(false);
         }
-        return super.onOptionsItemSelected(item);
+
     }
+
+    public void onSplashButtonClick(View v) {
+        Intent intent = new Intent(this, DirectionActivity.class);
+        startActivity(intent);
+    }
+
+    private boolean isGoogleServicesOk() {
+        int flag = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        switch (flag) {
+            case (ConnectionResult.SUCCESS): {
+                return true;
+            }
+            case (ConnectionResult.SERVICE_MISSING): {
+
+            }
+
+            case (ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED): {
+
+            }
+
+            case (ConnectionResult.SERVICE_DISABLED): {
+
+            }
+        }
+        Toast.makeText(this, "" + flag, Toast.LENGTH_SHORT).show();
+        GooglePlayServicesUtil.getErrorDialog(flag, this, REQUEST_CODE);
+        return false;
+    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//
+//        switch (requestCode) {
+//            case(REQUEST_CODE):{
+//                if (resultCode== Activity.RESULT_OK){
+//
+//                }
+//            }
+//        }
+//    }
+
+
+    //
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.my, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 }
